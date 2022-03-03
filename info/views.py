56ddfbc13f8af4c225f337a82f4bb296d9887d5c
Taras Datsiuk <taras.datsiuk@lifeisgoodlabs.com>
2022-03-03 12:49:03 +0200

    add page for countries

--- info/views.py
+++ info/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from .models import News, Crossing
+from .models import News, Country, Crossing
 from django.template import loader
 # Create your views here.
 
@@ -14,9 +13,24 @@ def news(request):
     }
     return HttpResponse(template.render(context, request))
 
+def countries(request):
+    country_list = Country.objects.order_by('order')
+    template = loader.get_template('info/countries.html')
+    context = {
+        'country_list': country_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def country(request, country_id):
+    country = Country.objects.get(id=country_id)
+    template = loader.get_template('info/country.html')
+    context = {
+        'country': country,
+    }
+    return HttpResponse(template.render(context, request))
+
 def crossings(request):
     crossings_list = Crossing.objects.order_by('order')
-    # template = loader.get_template('base.html')
     template = loader.get_template('info/crossings.html')
     context = {
         'crossings_list': crossings_list,
